6642487d3d16862e426ef91019bd7b0f4cca896f
tess-demos <tess.on.medium@gmail.com>
2020-05-24 11:41:16 +1000

    user Schema, config, and user register validations

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator/check')
 const router = express.Router()
+
+const User = require('../models/User')
 
 // @route           POST api/users
 // @description     Register a user
 // @access          Public 
-router.post('/', (req, res) => {
-    res.send('Register a user')
+router.post('/', [
+    check('name', 'please add a name')
+        .not()
+        .isEmpty(),
+    check('email', 'please include a valid email')
+        .isEmail(),
+    check(
+        'password', 
+        'please enter a password with 6 or more characters'
+        ).isLength({ min: 6})
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()})
+    }
+    res.send('passed validation')
 })
 
 module.exports = router
